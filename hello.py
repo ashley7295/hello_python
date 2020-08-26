5d87d79f9f8d8828f2377ca2c08fb38704812b92
ashley7295 <avkellerman@yahoo.com>
2020-08-25 19:43:23 -0500

    python practice and commit

--- hello.py
+++ hello.py
@@ -2,17 +2,18 @@
 #variables
 
 school = 'MCTC'
-
+gpa = 55
 
 # if-statements
 if gpa == 4:
     print ('WOW!')
 elif gpa >3:
-        print ('great!')
-else print ('still good!')
+    print ('great!')
+else:
+    print ('still good!')
 
 # lists
-school = ['mctc'. 'mankato', 'north hennepin']
+schools = ['mctc', 'mankato', 'north hennepin']
 
 if 'mctc' in schools:
     print ('MCTC is one of the schools in the list')

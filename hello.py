2278d4af640b905a8ed001bff69a1c8dc3cf9ba0
ashley7295 <avkellerman@yahoo.com>
2020-08-25 19:39:51 -0500

    python practice and commit test

--- hello.py
+++ hello.py
@@ -1,3 +1,19 @@
-print ("hello")
-print ("test commit 2")
-print ("test commit again")
+
+#variables
+
+school = 'MCTC'
+
+
+# if-statements
+if gpa == 4:
+    print ('WOW!')
+elif gpa >3:
+        print ('great!')
+else print ('still good!')
+
+# lists
+school = ['mctc'. 'mankato', 'north hennepin']
+
+if 'mctc' in schools:
+    print ('MCTC is one of the schools in the list')
+

9b4468b7c31c41d92ea0117046bf52f9d321e0ee
ashley7295 <avkellerman@yahoo.com>
2020-09-08 17:30:00 -0500

    welcome banner

--- camelCase.py
+++ camelCase.py
@@ -7,7 +7,16 @@ def camelcase(sentence):
 
     return upper_camel_cased[0:1].lower() +upper_camel_cased[1:]
 
+
+def banner():
+    #display welcome banner
+    message = "CAMELCASE PROGRAM!"
+    stars = "*" * len(message)
+    print (f"{stars}\n{message}\n{stars}")
+
+
 def main():
+    banner()
     sentence = input("Enter Your Sentence: ")
     output = camelcase(sentence)
     print(output)
